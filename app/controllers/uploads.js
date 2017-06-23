e57d8ce7eb10033ffe75ac67e2a74fb034651d22
Ben Adamski <benjadamski@gmail.com>
2017-06-23 12:43:58 -0400

    Refactors reusable pieces of bin/ script into lib/
    Pulled out all of the code pertaining to JUST the s3 upload piece
    and put it into a lib/ file. Now this lib/ package can be used from
    both the node script and our controller action by requiring it
    and calling `awsUpload(file)`
    
    https://cl.ly/lIES

---
+++ app/controllers/uploads.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const controller = require('lib/wiring/controller')
+const models = require('app/models')
+const Upload = models.upload
+
+const setModel = require('./concerns/set-mongoose-model')
+
+const multer = require('multer')
+const multerUpload = multer({ dest: '/tmp/' })
+
+const awsUpload = require('lib/aws-upload')
+
+const index = (req, res, next) => {
+  Upload.find()
+    .then(uploads => res.json({
+      uploads: uploads.map((e) =>
+        e.toJSON({ virtuals: true }))
+    }))
+    .catch(next)
+}
+
+const show = (req, res) => {
+  res.json({
+    upload: req.upload.toJSON({ virtuals: true })
+  })
+}
+
+const destroy = (req, res, next) => {
+  req.upload.remove()
+    .then(() => res.sendStatus(204))
+    .catch(next)
+}
+
+const create = (req, res, next) => {
+  const upload = {
+    path: req.file.path,
+    title: req.body.image.title
+  }
+  awsUpload(file)
+    .then((s3Response) => {
+      return Upload.create({
+        url: s3Response.Location,
+        title: s3Response.Key
+      })
+    })
+    .then((upload) => res.status(201).json({upload}))
+    .catch(next)
+}
+
+const update = (req, res, next) => {
+  req.upload.update(req.body.upload)
+    .then(() => res.sendStatus(204))
+    .catch(next)
+}
+
+module.exports = controller({
+  index,
+  show,
+  create,
+  update,
+  destroy
+}, { before: [
+  { method: multerUpload.single('image[file]'), only: ['create'] },
+  { method: setModel(Upload), only: ['show', 'destroy', 'update'] },
+  // { method: setModel(Upload), only: ['update', 'destroy'] }
+] })
